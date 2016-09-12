58d3c90d53d680a0fa7f91214bed2e59aa803918
J0eCool <count.j0ecool@gmail.com>
2016-09-11 22:40:54 -0700

    Player can jump

--- player.nim
+++ player.nim
@@ -8,19 +8,23 @@ type Player* = ref object of GameObject
 
 proc newPlayer*(): Player =
   Player(
-    pos: vec(500, 200),
+    pos: vec(500, 700),
     size: vec(80, 100),
     color: color(55, 38, 255, 255),
   )
 
 const
   speed = 280.0
-  accel = 1400.0
-  gravity = 240.0
+  accel = 1_400.0
+  gravity = 2_100.0
+  jumpHeight = 500.0
+  jumpSpeed = -sign(gravity).float * sqrt(2 * jumpHeight * abs(gravity))
+
 
 proc update*(player: Player, dt: float, input: InputManager) =
   if input.isPressed(Input.jump):
     player.started = true
+    player.vel.y = jumpSpeed
 
   var inputDir = 0
   if input.isHeld(Input.left):
@@ -35,7 +39,13 @@ proc update*(player: Player, dt: float, input: InputManager) =
   player.vel.x = clamp(player.vel.x, -speed, speed)
   if inputDir == 0 and preSign != sign(player.vel.x):
     player.vel.x = 0
-  if player.vel.x != 0:
-    echo player.vel.x
+
+  if player.started:
+    player.vel.y += gravity * dt
+    if player.vel.y < 0 and not input.isHeld(Input.jump):
+      player.vel.y += 2 * gravity * dt
+  if player.pos.y >= 900 and player.vel.y > 0:
+    player.vel.y = jumpSpeed
+
   let toMove = player.vel * dt
   player.move(toMove)
