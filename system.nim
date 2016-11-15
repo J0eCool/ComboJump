e52376f0c4c67ef49c43643748fe1d39e88641c0
J0eCool <count.j0ecool@gmail.com>
2016-11-15 00:35:52 -0800

    Add metadata writing to system macro

--- system.nim
+++ system.nim
@@ -1,9 +1,56 @@
-import macros
+import
+  algorithm,
+  macros,
+  parseutils,
+  strutils,
+  tables
 
-import entity
+import
+  entity,
+  util
+
+const sysFile = "systems.json"
+type Data = Table[string, int]
+proc readData(): Data =
+  result = initTable[string, int](64)
+  let
+    inStr = readFile(sysFile).string
+    lines = inStr.split("\n")
+  for ln in lines:
+    let parts = ln.split(":")
+    if parts.len > 1:
+      let
+        name = parts[0]
+        count = parts[1].parseInt
+      result[name] = count
+
+proc writeData(data: Data) =
+  var outStr = ""
+  for k, v in data.pairs:
+    outStr &= k & ":" & $v & "\n"
+  writeFile(sysFile, outStr)
+
+proc getNextId(data: Data): int =
+  var ids: seq[int] = @[]
+  for d in data.values:
+    ids.add d
+  ids.sort(cmp)
+  for x in ids:
+    if x > result:
+      return
+    result += 1
 
 macro defineSystem*(body: untyped): untyped =
+  var data = readData()
+  let key = $body[0].name
+  if not data.hasKey(key):
+    let nextId = getNextId(data)
+    data[key] = nextId
+
   var ps = body[0].params
   ps[0] = ident("Events")
   ps.insert 1, newIdentDefs(ident("entities"), ident("Entities"))
+
+  writeData(data)
+
   return body
