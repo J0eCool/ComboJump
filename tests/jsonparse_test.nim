978d90256a5321e04a6e8ae2b5d46db34a5783df
J0eCool <count.j0ecool@gmail.com>
2017-01-20 11:37:21 -0800

    Add JSON Tables test, whitespace reading test

--- tests/jsonparse_test.nim
+++ tests/jsonparse_test.nim
@@ -1,4 +1,6 @@
-import unittest
+import
+  tables,
+  unittest
 
 import
   jsonparse
@@ -7,7 +9,7 @@ type TestEnum = enum
   one
   two
 
-template roundtrip(name: string, ty, value, jsonString: untyped): untyped =
+template testRoundtrip(name: string, ty, value, jsonString: untyped): untyped =
   test ($name & " toJSON"):
     check $toJSON(value) == jsonString
 
@@ -15,14 +17,53 @@ template roundtrip(name: string, ty, value, jsonString: untyped): untyped =
     check fromJSON[ty](deserializeJSON(jsonString)) == value
 
 suite "JSON parsing":
-  roundtrip("Int", int, 127, "\"127\"")
-  roundtrip("Negative Int", int, -7, "\"-7\"")
-  roundtrip("String", string, "String literal.", "\"String literal.\"")
-  roundtrip("Enum", TestEnum, one, "\"one\"")
-  roundtrip("Seq", seq[int], @[8, 0, 2, 3], """["8","0","2","3"]""")
+  testRoundtrip(
+    "Int", int,
+    127,
+    "\"127\""
+  )
 
-  # TODO: dicts, objects
-  # TODO: check skipping whitespace, pretty-printing
+  testRoundtrip(
+    "Negative Int", int,
+    -7,
+    "\"-7\""
+  )
+
+  testRoundtrip(
+    "String", string,
+    "String literal.",
+    "\"String literal.\""
+  )
+
+  testRoundtrip(
+    "Enum", TestEnum,
+    one,
+    "\"one\""
+  )
+
+  testRoundtrip(
+    "Array", seq[int],
+    @[8, 0, 2, 3],
+    """["8","0","2","3"]"""
+  )
+
+  testRoundtrip(
+    "Nested array", seq[seq[int]],
+    @[@[1, 2], @[3, 4]],
+    """[["1","2"],["3","4"]]"""
+  )
+
+  var testTable = initTable[string, int]()
+  testTable["foo"] = 12
+  testTable["Bar"] = 101
+  testRoundtrip(
+    "Tables", Table[string, int],
+    testTable,
+    """{"Bar":"101","foo":"12"}""" # TODO: make ordering of object keys deterministic
+  )
+
+  test "Whitespace is ignored":
+    check fromJSON[seq[int]](deserializeJSON(" [\"1\", \"2\",\n \"3\" ] ")) == @[1, 2, 3]
 
 # TODO: have non-aborting fromJSON failures
   # echo "fails: ", fromJSON[TestEnum](JSON(kind: jsString, str: "three"))
