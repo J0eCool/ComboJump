c9b74ba36dfc2021d8cc89ca02d8b73220e2a8c0
J0eCool <count.j0ecool@gmail.com>
2017-02-02 21:08:25 -0800

    Move roomEntities proc out to its own file

--- system/update_stage_select.nim
+++ system/update_stage_select.nim
@@ -5,7 +5,6 @@ import
 import
   component/[
     collider,
-    room_camera_target,
     sprite,
     target_shooter,
     transform,
@@ -16,6 +15,9 @@ import
     rune_menu,
     spell_hud_menu,
     stats_menu,
+  ],
+  nano_mapgen/[
+    room,
   ],
   input,
   entity,
@@ -28,69 +30,6 @@ import
   stages,
   vec,
   util
-
-proc wall(pos = vec(), size = vec(), hasDoor = false): Entities =
-  let
-    wallColor = color(128, 128, 128, 255)
-    doorWidth = 200.0
-    name = "Wall"
-  if not hasDoor:
-    return @[newEntity(name, [
-      Sprite(color: wallColor),
-      Collider(layer: Layer.floor),
-      Transform(pos: pos, size: size)
-    ])]
-
-  let
-    # isVertical = size.x < size.y
-    offset = vec(size.x / 4 + doorWidth / 4, 0.0)
-    leftPos = pos - offset
-    rightPos = pos + offset
-    partSize = vec((size.x - doorWidth) / 2, size.y)
-  @[
-    newEntity(name, [
-      Sprite(color: wallColor),
-      Collider(layer: Layer.floor),
-      Transform(pos: leftPos, size: partSize)
-    ]),
-    newEntity(name, [
-      Sprite(color: wallColor),
-      Collider(layer: Layer.floor),
-      Transform(pos: rightPos, size: partSize)
-    ]),
-  ]
-
-
-proc roomEntities(screenSize, pos: Vec): Entities =
-  let wallWidth = 50.0
-  result = @[
-    newEntity("Room", [
-      RoomCameraTarget(),
-      Collider(layer: Layer.playerTrigger),
-      Transform(
-        pos: pos + screenSize / 2,
-        size: screenSize - vec(2 * wallWidth),
-      ),
-    ]),
-  ]
-  result &= wall( # left
-    pos = pos + vec(wallWidth / 2, screenSize.y / 2),
-    size = vec(wallWidth, screenSize.y - 2 * wallWidth),
-  )
-  result &= wall( # right
-    pos = pos + vec(screenSize.x - wallWidth / 2, screenSize.y / 2),
-    size = vec(wallWidth, screenSize.y - 2 * wallWidth),
-  )
-  result &= wall( # top
-    pos = pos + vec(screenSize.x / 2, wallWidth / 2),
-    size = vec(screenSize.x, wallWidth),
-    hasDoor = true,
-  )
-  result &= wall( # bottom
-    pos = pos + vec(screenSize.x / 2, screenSize.y - wallWidth / 2),
-    size = vec(screenSize.x, wallWidth),
-    hasDoor = true,
-  )
 
 proc spawnedEntities*(stage: Stage, player: Entity): Entities =
   let
