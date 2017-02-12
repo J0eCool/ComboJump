bd90a5bc9e9cf773944014c8ab1086c419719184
J0eCool <count.j0ecool@gmail.com>
2017-02-11 22:17:02 -0800

    Copy map pathfinding to graph

--- nano_mapgen/map_desc.nim
+++ nano_mapgen/map_desc.nim
@@ -1,3 +1,5 @@
+import hashes, tables
+
 import
   nano_mapgen/[
     map,
@@ -10,12 +12,27 @@ type
     door: DoorKind
     node: MapNode
   MapNode = ref object
+    id: int
     kind: RoomKind
     edges: seq[Edge]
   MapGraph = object
     nodes: seq[MapNode]
+    startNode: MapNode
+    endNode: MapNode
   MapDesc* = object
     length*: int
+
+var nextMapId = 0
+proc newMapNode(kind = roomNormal): MapNode =
+  result = MapNode(
+    id: nextMapId,
+    kind: kind,
+    edges: @[],
+  )
+  nextMapId += 1
+
+proc hash(node: MapNode): Hash =
+  node.id.hash
 
 proc edgeFor(node, neighbor: MapNode): Edge =
   for edge in node.edges:
@@ -25,9 +42,9 @@ proc edgeFor(node, neighbor: MapNode): Edge =
 
 proc connect(a, b: MapNode) =
   if a.edgeFor(b) == nil:
-    a.edges.safeAdd Edge(door: doorOpen, node: b)
+    a.edges.add Edge(door: doorOpen, node: b)
   if b.edgeFor(a) == nil:
-    b.edges.safeAdd Edge(door: doorOpen, node: a)
+    b.edges.add Edge(door: doorOpen, node: a)
 
 proc split(a, b: MapNode): MapNode =
   let
@@ -36,7 +53,7 @@ proc split(a, b: MapNode): MapNode =
   if e1 == nil or e2 == nil:
     return nil
 
-  result = MapNode()
+  result = newMapNode()
   a.edges.remove(e1)
   b.edges.remove(e2)
   connect(a, result)
@@ -44,41 +61,63 @@ proc split(a, b: MapNode): MapNode =
 
 proc generateNodes(desc: MapDesc): MapGraph =
   let
-    startNode = MapNode(kind: roomStart)
-    endNode = MapNode(kind: roomEnd)
+    startNode = newMapNode(roomStart)
+    endNode = newMapNode(roomEnd)
   connect(startNode, endNode)
   var nodes = @[startNode, endNode]
   for i in 0..<desc.length - 2:
     nodes.add split(startNode, startNode.edges[0].node)
-  MapGraph(nodes: nodes)
+  MapGraph(
+    nodes: nodes,
+    startNode: startNode,
+    endNode: endNode,
+  )
 
-proc start(graph: MapGraph): MapNode =
-  for node in graph.nodes:
-    if node.kind == roomStart:
-      return node
+proc findPath(a, b: MapNode): seq[MapNode] =
+  var
+    openSet = @[a]
+    visited = initTable[MapNode, MapNode]()
+  visited[a] = nil
+
+  while openSet.len > 0:
+    let cur = openSet[0]
+    openSet.delete(0)
+    for e in cur.edges:
+      let next = e.node
+      if not visited.hasKey(next):
+        openSet.add(next)
+        visited[next] = cur
+        if next == b:
+          break
+
+  if not visited.hasKey(b):
+    return @[]
+
+  result = @[b]
+  while result[0] != a:
+    let cur = result[0]
+    assert(visited.hasKey(cur),
+      "Backtracing path finds parent that wasn't visited")
+    let prev = visited[cur]
+    assert(not (prev in result), "Cycle found when backtracing path")
+    result.insert(prev, 0)
 
 proc generate*(desc: MapDesc): Map =
   var
-    curNode = desc.generateNodes().start()
+    graph = desc.generateNodes()
     rooms = newSeq[Room]()
-    visited = newSeq[MapNode]()
-  while curNode != nil:
+    path = findPath(graph.startNode, graph.endNode)
+  for i in 0..<path.len:
     let
+      node = path[i]
       id = rooms.len + 1
       next = Room(
         id: id,
-        kind: curNode.kind,
+        kind: node.kind,
         x: 0,
         y: id - 1,
         up: doorOpen,
         down: doorOpen,
       )
     rooms.add next
-    visited.add curNode
-    var nextNode: MapNode = nil
-    for e in curNode.edges:
-      if not (e.node in visited):
-        nextNode = e.node
-        break
-    curNode = nextNode
   Map(rooms: rooms)
