0c82f1497198449d4858c3d418a354451689417b
J0eCool <count.j0ecool@gmail.com>
2017-06-14 21:05:08 -0700

    Add some failing physics tests to start fixing

--- tests/physics_test.nim
+++ tests/physics_test.nim
@@ -72,6 +72,11 @@ suite "Collisions":
     discard physics(@[player, singleBlock], dt)
     check player.pos.approxEq(vec(10, -30))
 
+  test "Can't skip collision with excessive velocity":
+    let player = testPlayer(vec(0, -50), vec(0, 5000))
+    discard physics(@[player, singleBlock], dt)
+    check player.pos.approxEq(vec(0, -30))
+
   test "onGround false when not on ground":
     let player = testPlayer(vec(50, 0), vec(0))
     discard physics(@[player, singleBlock], dt)
@@ -128,6 +133,13 @@ suite "Collisions - TileRoom":
       @[oo,xX,oo],
       @[oo,oo,oo],
     ])
+    boxRoom = tileGridEntity(vec(-40), vec(20), @[
+      @[xX,xX,xX,xX,xX],
+      @[xX,oo,oo,oo,xX],
+      @[xX,oo,oo,oo,xX],
+      @[xX,oo,oo,oo,xX],
+      @[xX,xX,xX,xX,xX],
+    ])
     dt = 1.0
 
   test "No movement with no velocity":
@@ -155,6 +167,11 @@ suite "Collisions - TileRoom":
     discard physics(@[player, singleBlock], dt)
     check player.pos.approxEq(vec(10, -30))
 
+  test "Can't skip collision with excessive velocity":
+    let player = testPlayer(vec(0, -50), vec(0, 5000))
+    discard physics(@[player, singleBlock], dt)
+    check player.pos.approxEq(vec(0, -30))
+
   test "onGround false when not on ground":
     let player = testPlayer(vec(50, 0), vec(0))
     discard physics(@[player, singleBlock], dt)
@@ -166,3 +183,17 @@ suite "Collisions - TileRoom":
       vec(0.0, 50 * gravitySign))
     discard physics(@[player, singleBlock], dt)
     check player.onGround
+
+  test "Multiple tiles don't stop movement":
+    let
+      playerRight = testPlayer(vec(-10,  20), vec( 20,   5))
+      playerLeft =  testPlayer(vec( 10, -20), vec(-20,  -5))
+      playerDown =  testPlayer(vec( 20, -10), vec(  5,  20))
+      playerUp =    testPlayer(vec(-20,  10), vec( -5, -20))
+      entities = @[boxRoom, playerLeft, playerRight, playerDown, playerUp]
+    discard physics(entities, dt)
+    check:
+      playerRight.pos.approxEq vec( 10,  20)
+      playerLeft.pos.approxEq  vec(-10, -20)
+      playerDown.pos.approxEq  vec( 20, -10)
+      playerUp.pos.approxEq    vec(-20,  10)
