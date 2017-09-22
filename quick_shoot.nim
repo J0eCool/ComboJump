49b6e7d222b3099f354f7738c3051ccaa27520e6
J0eCool <count.j0ecool@gmail.com>
2017-09-22 14:52:20 -0700

    Add outline for QuickShoot game

---
+++ quick_shoot.nim
@@ -0,0 +1,43 @@
+import math, macros, times
+from sdl2 import RendererPtr
+
+const Profile {.intdefine.}: int = 0
+when Profile != 0:
+  import nimprof
+
+import
+  component/collider,
+  quick_shoot/[
+    title_screen,
+  ],
+  game,
+  menu,
+  program,
+  vec
+
+
+type QuickShootGame* = ref object of Game
+  menu: Node
+
+proc newQuickShootGame*(screenSize: Vec): QuickShootGame =
+  new result
+  result.camera.screenSize = screenSize
+  result.title = "Quick Shooter"
+
+method loadEntities*(game: QuickShootGame) =
+  game.entities = @[]
+  game.menus.push newTitleMenu()
+
+method draw*(renderer: RendererPtr, game: QuickShootGame) =
+  renderer.drawGame(game)
+
+  renderer.draw(game.menus, game.resources)
+
+method update*(game: QuickShootGame, dt: float) =
+  game.dt = dt
+
+  game.menus.update(dt, game.input)
+
+when isMainModule:
+  let screenSize = vec(1200, 900)
+  main(newQuickShootGame(screenSize), screenSize)
---
+++ quick_shoot/title_screen.nim
@@ -0,0 +1,41 @@
+import
+  menu,
+  vec
+
+
+type
+  TitleScreen = ref object of RootObj
+  TitleScreenController = ref object of Controller
+    start: bool
+
+method pushMenus(controller: TitleScreenController): seq[MenuBase] =
+  if controller.start:
+    controller.start = false
+    # let levelSelect = downcast(newLevelSelectMenu())
+    # result = @[downcast(newTransitionMenu(levelSelect))]
+
+proc titleScreenView(menu: TitleScreen, controller: TitleScreenController): Node {.procvar.} =
+  Node(
+    children: @[
+      BorderedTextNode(
+        pos: vec(600, 150),
+        text: "Quick Shooter",
+        fontSize: 72,
+      ),
+      Button(
+        pos: vec(600, 700),
+        size: vec(300, 120),
+        children: @[BorderedTextNode(text: "START").Node],
+        onClick: (proc() =
+          controller.start = true
+        ),
+      ),
+    ],
+  )
+
+proc newTitleMenu*(): Menu[TitleScreen, TitleScreenController] =
+  Menu[TitleScreen, TitleScreenController](
+    model: TitleScreen(),
+    view: titleScreenView,
+    controller: TitleScreenController(),
+  )
